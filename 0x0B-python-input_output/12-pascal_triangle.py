ca0a31c9dbb0737e023450a8b42c69944505190b
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-09-28 08:58:14 -0700

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-'''Module for pascal_triangle method.'''
+"""Documentation for a pascal_triangle function"""
 
 
 def pascal_triangle(n):
-    '''Method that solves pascal's triangle.'''
-    rows = [[1 for j in range(i + 1)] for i in range(n)]
-    for n in range(n):
-        for i in range(n - 1):
-            rows[n][i + 1] = sum(rows[n - 1][i:i + 2])
-    return rows
+    """Prints a list of lists of a pascal triangle
+    Args:
+        n (int): the number of row of the Pascal Triangle
+    Returns:
+        The Pascal Triangle as a list of lists
+    """
+
+    triangle = []
+    prev_row = [1]
+    row = 0
+    if n <= 0:
+        return []
+    triangle.append(prev_row)
+    if n == 1:
+        return triangle
+    while row < n - 1:
+        current_row = []
+        current_row.append(prev_row[0])
+        i = 0
+        while i < row:
+            current_row.append(prev_row[i] + prev_row[i + 1])
+            i += 1
+        current_row.append(prev_row[len(prev_row) - 1])
+        triangle.append(current_row)
+        prev_row = current_row
+        row += 1
+    return triangle
