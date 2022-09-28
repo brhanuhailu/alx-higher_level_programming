89784db48ea09714a43141d8ea8b93a02435706e
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-09-28 09:17:23 -0700

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-'''Module for saving argv info via json to file.'''
-import json
-import os.path
-import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+from os import path
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = "add_item.json"
-json_list = []
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
+if path.exists('add_item.json'):
+    obj_json_file = load_from_json_file('add_item.json')
+else:
+    obj_json_file = []
 
-for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
+for i in range(1, len(argv)):
+    obj_json_file.append(argv[i])
 
-save_to_json_file(json_list, file)
+save_to_json_file(obj_json_file, 'add_item.json')
