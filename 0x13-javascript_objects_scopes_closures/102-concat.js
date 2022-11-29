148fdd7e60eada1aeccb439b6924403bb7eaf715
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-11-29 05:08:47 -0800

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-/*
-The first argument is the file path of the first source file
-The second argument is the file path of the second source file
-The third argument is the file path of the destination
-*/
-const files = require('files');
-const file1 = files.readFileSync(process.argv[2], 'utf8');
-const file2 = files.readFileSync(process.argv[3], 'utf8');
-files.writeFileSync(process.argv[4], file1 + file2);
+// script that concats 2 files
+
+const args = process.argv.slice(2);
+const file = require('fs');
+const contentA = file.readFileSync('./' + args[0]);
+const contentB = file.readFileSync('./' + args[1]);
+file.writeFileSync('./' + args[2], contentA + contentB);
