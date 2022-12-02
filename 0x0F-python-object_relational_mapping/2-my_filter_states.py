c60ab403cbb22144c75af2348181624fb132a210
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-12-02 12:44:01 -0800

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
-"""Filter states by user input"""
+"""
+Takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+
+from sys import argv
 import MySQLdb
 
-
-def main():
-
-    # connect
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    # cursor
-    c = db.cursor()
-
-    # execute query
-    c.execute("SELECT * FROM states WHERE BINARY name = '{:s}'\
-    ORDER BY id ASC".format(argv[4]))
-
-    # fetch
-    rows = c.fetchall()
-
-    # print
-    for row in rows:
-        print(row)
-
-    # close
-    c.close()
-    db.close()
-
 if __name__ == "__main__":
-    from sys import argv
-    main()
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset='utf8')
+    cur = db.cursor()
+    cur.execute("SELECT *\
+                FROM `states` WHERE BINARY `name` = '{}'".format(argv[4]))
+    [print(state) for state in cur.fetchall()]
