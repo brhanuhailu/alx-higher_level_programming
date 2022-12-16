00efeb689a8ce775cc741c4cc454e55b25ef0e77
brhanuhailu <73739300+brhanuhailu@users.noreply.github.com>
2022-12-16 06:53:35 -0800

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@ Script to check status
 if __name__ == "__main__":
     import requests
 
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
